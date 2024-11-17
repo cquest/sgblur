e063e1a1f57cdf8df65f97bc9f9ec911bb70bd98
cquest <cquest@openstreetmap.fr>
2024-11-17 20:54:55 +0100

    split large equirectangular pictures (2:1 ratio) to reduce required GPU VRAM

--- src/detect/detect.py
+++ src/detect/detect.py
@@ -6,6 +6,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from ultralytics import YOLO
 import turbojpeg
 import json
+from PIL import Image
 
 jpeg = turbojpeg.TurboJPEG()
 model = YOLO("./models/yolo11l_panoramax.pt")
@@ -29,6 +30,12 @@ def detector(picture, cls=''):
 
     # copy received JPEG picture to temporary file
     tmp = '/dev/shm/detect%s.jpg' % pid
+    tmp_left = '/dev/shm/detect%sL.jpg' % pid
+    tmp_right = '/dev/shm/detect%sR.jpg' % pid
+    src = [tmp]
+    result = []
+    split = 0
+    offset = []
 
     with open(tmp, 'w+b') as jpg:
         jpg.write(picture.file.read())
@@ -40,26 +47,46 @@ def detector(picture, cls=''):
 
     # call our detection model on reduced image to detect large close-up objects
     try:
-        results = model.predict(source=tmp, conf=0.05, imgsz=1024, half=True, verbose=False)
-        result = [results[0]]
-        offset = [[0,0]]
+        results = model.predict(src, conf=0.20, imgsz=1024, half=True, verbose=False)
+        result.append(results[0])
+        offset.append(0)
+        if split>0:
+            result.append(results[1])
+            offset.append(split)
     except:
         return None
 
     # detect again with standard resolution
     try:
-        results = model.predict(source=tmp, conf=0.05, imgsz=2048, half=True, verbose=False)
+        results = model.predict(src, conf=0.20, imgsz=2048, half=True, verbose=False)
         result.append(results[0])
-        offset.append([0,0])
+        offset.append(0)
+        if split>0:
+            result.append(results[1])
+            offset.append(split)
     except:
         return None
 
     if width>=5760:
+        # panoramic / 360° pictures
+        if width >= height * 2:
+            # split image in left and right parts
+            split = int(width/2)
+            with Image.open(tmp) as img_left:
+                img_left.crop((0,0,split-1,height)).save(tmp_left)
+            with Image.open(tmp) as img_right:
+                img_right.crop((split,0,width,height)).save(tmp_right)
+            src = [tmp_left, tmp_right]
+
         # detect again at higher resolution for smaller objects
         try:
-            results = model.predict(source=tmp, conf=0.05, imgsz=min(int(width) >> 5 << 5,4096), half=True, verbose=False)
+            results = model.predict(source=src[0], conf=0.20, imgsz=min(int(width) >> 5 << 5,3840), half=True, verbose=False)
             result.append(results[0])
-            offset.append([0,0])
+            offset.append(0)
+            if split>0:
+                results = model.predict(source=src[1], conf=0.20, imgsz=min(int(width) >> 5 << 5,3840), half=True, verbose=False)
+                result.append(results[0])
+                offset.append(split)
         except:
             return None
 
@@ -78,15 +105,15 @@ def detector(picture, cls=''):
             if cls !='' and not names[int(obj.cls)] in cls:
                 continue
             box = obj.xywh
-            box_l = int(offset[r][0] + box[0][0] - box[0][2] * 0.5) >> hblock << hblock
-            box_t = int(offset[r][0] + box[0][1] - box[0][3] * 0.5) >> vblock << vblock
+            box_l = int(offset[r] + box[0][0] - box[0][2] * 0.5) >> hblock << hblock
+            box_t = int(box[0][1] - box[0][3] * 0.5) >> vblock << vblock
             box_w = int(box[0][2]) + (2 << hblock) >> hblock << hblock
             if names[int(obj.cls)] == 'sign':
                 box_h = int(box[0][3] * 1.25 + (2 << vblock)) >> vblock << vblock
             else:
                 box_h = int(box[0][3]) + (2 << vblock) >> vblock << vblock
 
-            # remove overlaping crops
+            # remove overlaping detections
             crop = [ max(0,box_l),
                     max(0,box_t),
                     min(box_w, width-max(0,box_l)),
@@ -107,9 +134,9 @@ def detector(picture, cls=''):
                     "xywh": crop_rects[-1]
                 })
                 bbox.append({   'top': int(box[0][1]),
-                                'left': int(box[0][0]),
+                                'left': int(offset[r]+box[0][0]),
                                 'bottom': int(box[0][1]+box[0][3]),
-                                'right': int(box[0][0]+box[0][2]),
+                                'right': int(offset[r]+box[0][0]+box[0][2]),
                                 'cls': names[int(obj.cls)],
                                 'conf': round(float(obj.conf),3)
                             })
